fad298a9a621d15f8cc714098373703dfbab2621
Kronos66 <maksymilian.pamula@gmail.com>
2014-09-03 14:30:47 +0200

    made exercise 6 finish

--- app/ReservationCtrl.js
+++ app/ReservationCtrl.js
@@ -39,9 +39,20 @@
                 date: ctrl.list.date,
                 comments: ctrl.list.comments
             };
-        }
+        };
+        ctrl.showStatusRefreshment = function () {
+            var selected = $filter('filter')(ctrl.refreshments, {value: ctrl.list.selectRefreshment});
+            return (ctrl.list.selectRefreshment && selected.length) ? selected[0].status : 'Not set';
+        };
+
+
+        $scope.$watchCollection('[reservation.list.firstName, reservation.list.lastName,reservation.list.email,reservation.list.selectRefreshment,' +
+            'reservation.list.selectZone,reservation.list.vip,reservation.list.date,reservation.list.comments]', function () {
+            ctrl.save()
+        });
     }
 
-    var module = angular.module('exerciseApp', ["xeditable"]);
+
+    var module = angular.module('exerciseApp', ["xeditable", "ui.bootstrap"]);
     module.controller('ReservationCtrl', ['$filter','$scope', ReservationCtrl]);
 })();
