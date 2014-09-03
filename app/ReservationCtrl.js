2fbf8cda80c4e4bb5bab96b8e706c6e5688c9007
Kronos66 <maksymilian.pamula@gmail.com>
2014-09-03 21:43:04 +0200

    made exercise 6 finish

--- app/ReservationCtrl.js
+++ app/ReservationCtrl.js
@@ -27,7 +27,7 @@
         };
 
         ctrl.formData = {};
-
+        $scope.$watch(
         this.save = function(){
             ctrl.formData = {
                 firstName: ctrl.list.firstName,
@@ -38,18 +38,18 @@
                 vip: ctrl.list.vip,
                 date: ctrl.list.date,
                 comments: ctrl.list.comments
-            };
-        };
+            }
+        }, ctrl.save());
         ctrl.showStatusRefreshment = function () {
             var selected = $filter('filter')(ctrl.refreshments, {value: ctrl.list.selectRefreshment});
             return (ctrl.list.selectRefreshment && selected.length) ? selected[0].status : 'Not set';
         };
 
-
-        $scope.$watchCollection('[reservation.list.firstName, reservation.list.lastName,reservation.list.email,reservation.list.selectRefreshment,' +
-            'reservation.list.selectZone,reservation.list.vip,reservation.list.date,reservation.list.comments]', function () {
-            ctrl.save()
-        });
+//      Other solution
+//        $scope.$watchCollection('[reservation.list.firstName, reservation.list.lastName,reservation.list.email,reservation.list.selectRefreshment,' +
+//            'reservation.list.selectZone,reservation.list.vip,reservation.list.date,reservation.list.comments]', function () {
+//            ctrl.save()
+//        });
     }
 
 
