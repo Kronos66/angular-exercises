e2eac20fdf83652fd5eeb98aa495f9ea7b6200f2
Kronos66 <maksymilian.pamula@gmail.com>
2014-09-04 10:02:00 +0200

    made exercise 7 finish

--- app/scripts/controllers/typeahead.js
+++ app/scripts/controllers/typeahead.js
@@ -1,14 +1,19 @@
 'use strict';
 
-angular.module('itcApp').controller('TypeaheadCtrl', ['ui.bootstrap'], function ($filter, ContactDAO) {
-
+angular.module('itcApp').controller('TypeaheadCtrl', function ($filter, ContactDAO) {
+    var ctrl = this;
     this.selected = undefined;
-    ContactDAO.getAll();
-    this.getContacts=function(typedValue){
-	// need to write this function body
-    }
 
-    this.selectContact=function(item,model,label){
-        this.selected=item;
-    }
+    this.getContacts = function (typedValue) {
+        ContactDAO.getAll().then(function (data) {
+            ctrl.data = data[typedValue];
+
+        });
+    };
+
+    this.selectContact = function (item, model, label) {
+        this.selected = item;
+    };
+
+
 });
